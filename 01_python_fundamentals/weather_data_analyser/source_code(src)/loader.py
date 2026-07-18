a3aa54b0d9a6beace9825ecf6bc934e0b5d61dc4
YAOU Elisee <265454115+yaouelisee@users.noreply.github.com>
2026-07-18 13:30:26 +0100

    add the loader file to analyze

---
+++ 01_python_fundamentals/weather_data_analyser/source_code(src)/loader.py
@@ -0,0 +1,34 @@
+def load_weather_data(file_path):
+
+    """create a funtion that reads the weather_data.txt file and converts it into a list of dictionnaries."""
+
+    # This variable will store all weather observations 
+    weather_records=[]
+
+    # This built-in opens the file as a read mode 
+    with open (file_path,"r") as file:
+
+        # Read the file one line at time 
+        for line in file :
+
+            # Remove spaces and the newline character at the end (/n)
+            line = line.strip()
+
+            # Split the line into seperate values using commas 
+            parts = line.split(",")
+
+            # Create a dictionnary representing one weather observation 
+            weather = {
+                "date"=parts[0], max_temp =int(parts[1]), min_temp=int(parts[2]), rainfall=int(parts[3]), humidity=int(parts[4]), wind_speed=int(parts[5])}
+
+            # Add the weather dictionnary into the empty list 
+            weather_records.append(weather)
+
+            # send the complete dataset back to the program 
+            return weather_records
+        
+
+
+
+        
+       
