0a7fd1bf98f697ffb37e7555c3c4eb2636173bf1
YAOU Elisee <265454115+yaouelisee@users.noreply.github.com>
2026-09-04 20:19:39 +0100

    Create the architecture of my new project : weather_observation_analyzer

--- 01_python_fundamentals/weather_data_analyzer/src/reporter.py
+++ 01_python_fundamentals/weather_data_analyzer/src/reporter.py
@@ -23,27 +23,27 @@ def generate_report(records,total,hottest_days,coldest_day,
 
     # :.2f formats a number to exactly 2 decimal places (e.g. 32.02)
     print(f"\nThe hottest day is  : {hottest_days['date']}"  )
-    print(f"The maximum temperature is :{hottest_days['max_temp']}")
+    print(f"The maximum temperature is :{hottest_days['max_temp']}C")
 
     print(f"\n the coldest day is :{coldest_day['date']}")
-    print(f"The minimum temperature is :{coldest_day['min_temp']}")
+    print(f"The minimum temperature is :{coldest_day['min_temp']}C")
 
     print(f"\n The rainiest day is :{rainiest_day['date']}")
-    print(f"The highest rainfall is : {rainiest_day['rainfall']}")
+    print(f"The highest rainfall is : {rainiest_day['rainfall']}mm")
 
     print(f"\nThe windest day is : {windest_day['date']}")
-    print(f"The highest wind speed is : {windest_day['wind_speed']}")
+    print(f"The highest wind speed is : {windest_day['wind_speed']}km/h")
 
 # Display every average of analyser.py 
-    print(f"\nThe average maximum temperature is :{average_maximum_temperature:.2f}")
+    print(f"\nThe average maximum temperature is :{average_maximum_temperature:.2f}C")
 
-    print(f"The average minimum temperature is :{average_minimum_temperature:.2f}")
+    print(f"The average minimum temperature is :{average_minimum_temperature:.2f}C")
 
-    print(f"The average rainfall is :{average_rainfall:.2f}")
+    print(f"The average rainfall is :{average_rainfall:.2f}mm")
 
-    print(f"The average humidity is :{average_humidity:.2f}")
+    print(f"The average humidity is :{average_humidity:.2f} %")
     
-    print(f"The average wind speed is :{average_wind_speed:.2f}")
+    print(f"The average wind speed is :{average_wind_speed:.2f}km/h")
 
 # Display the number of rainy days and the number of dry days 
     print(f"Number of rainy days :{count_rainy_days}")
@@ -54,7 +54,7 @@ def generate_report(records,total,hottest_days,coldest_day,
     print(f"\nThe day with the highest humidity is :{day_with_highest_humidity}")
 
 # Display the daily temperature range 
-    print(f"The daily temperature range is :{largest_range_day}")
+    print(f"The daily temperature range is :{largest_range_day}C")
 
 
     
